4c952944448114d217ecdf0d1a581a3608f55528
Niladri28dutta <nild2141@gmail.com>
2018-04-04 02:26:59 +0530

    - add proxy.conf.js for CORS proxy server

--- Angularcharts/src/app/linechart/linechart.component.ts
+++ Angularcharts/src/app/linechart/linechart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
 import {WeatherService} from '../weather.service';
-import {Chart} from 'chart.js';
-
+import Chart from 'chart.js';
+declare var Chart: any;
 @Component({
   selector: 'app-linechart',
   templateUrl: './linechart.component.html',
@@ -14,8 +14,50 @@ export class LinechartComponent implements OnInit {
 
   ngOnInit() {
     this._weather.dailyWeatherUpdate().subscribe(res => {
-      console.log(res);
-    })
+      //console.log(res);
+      let maxTemperature = res['list'].map(res => res.main.temp_max);
+      let minTemperature = res['list'].map(res => res.main.temp_min);
+      let allDatesinRange = res['list'].map(res => res.dt)
+      
+      let displayDates = [];
+
+      allDatesinRange.forEach((val:any) => {
+        let currDate = new Date(val * 1000);
+        displayDates.push(currDate.toLocaleTimeString('en',{ year: 'numeric', month: 'short', day: 'numeric' }));
+      });
+      let ctx = this.lineChartCanvas.nativeElement.getContext('2d');
+      this.chart = new Chart(ctx,{
+        type: 'line',
+        data: {
+          labels: displayDates,
+          datasets: [
+            { 
+              data: maxTemperature,
+              borderColor: "#3cba9f",
+              fill: false
+            },
+            { 
+              data: minTemperature,
+              borderColor: "#ffcc00",
+              fill: false
+            },
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      })
+    });
   }
 
 }
--- Angularcharts/src/app/weather.service.ts
+++ Angularcharts/src/app/weather.service.ts
@@ -1,11 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders,HttpClientModule} from '@angular/common/http';
 import 'rxjs/add/operator/map';
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/jsonp' })
-};
-
-httpOptions.headers.append('Access-Control-Allow-Origin' , '*');
 
 @Injectable()
 export class WeatherService {
@@ -13,7 +8,7 @@ export class WeatherService {
   constructor(private _http:HttpClient) { }
 
   dailyWeatherUpdate(){
-    return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22",httpOptions)
+    return this._http.get("api/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
                       .map((result:any) => result );
   }
 
