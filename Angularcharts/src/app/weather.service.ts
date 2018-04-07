f3e64a1b02fbd45eab8ba0722877d7ba450b112e
Niladri28dutta <nild2141@gmail.com>
2018-04-07 23:45:24 +0530

    - fix API

--- Angularcharts/src/app/weather.service.ts
+++ Angularcharts/src/app/weather.service.ts
@@ -9,7 +9,7 @@ export class WeatherService {
   constructor(private _http:HttpClient) { }
 
   dailyWeatherUpdate(){
-    return this._http.get(environment.API_URL+"api/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
+    return this._http.get(environment.API_URL+"/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
                       .map((result:any) => result );
   }
 
